ff1d7df2edf4dcd0c31d59fe1b8db9dfeab8ddc3
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-10 17:16:57 +0500

    routing from server added

--- app/routes/routes_note.js
+++ app/routes/routes_note.js
@@ -1,3 +1,9 @@
 module.exports = function(app, db) {
 
+    app.post('/notes', (req, res) => {
+        // You'll create 
+        console.log(req.body);
+        console.log('sending data to App');
+        res.send('Hello')
+    })
 };
