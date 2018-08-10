584b6ce647d29b0e5a4d95068efb86ceeff0ef20
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-10 17:17:54 +0500

    comments deleted

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
-const noteRoutes = require('./note_routes');
+const noteRoutes = require('./routes_note');
 
 module.exports = function(app, db) {
     noteRoutes(app, db);
-    // Other route groups could go here, in the future
+
   };
