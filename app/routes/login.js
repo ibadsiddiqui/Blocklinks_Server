c10dfaf5c54d46faa93cc73af94573e722fc2e5a
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-12 23:56:58 +0500

    sign in capability added to the server

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,9 +1,22 @@
 module.exports = function(app, db) {
 
-    app.post('/login', (req, res) => {
+    const Web3 = require('web3');
+    var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+    app.post('/login', async (req, res) => {
+        
+        
         // You'll create 
-        console.log(req.body);
-        console.log('sending data to App');
+        console.log('Logging IN');
+        console.log('Checking Email and Password');
+        console.log(req.body.email + " " + req.body.password);
+        var log = String(req.body.Email);
+        var password = String(req.body.Password);
+
+        web3.eth.personal.unlockAccount("0x634c9F966987b8e0dF385a6f94fB54FE1EdB4E94", log+password, 300, () => {
+            console.log('Account unlocked!')
+        });
+        
         res.send('Hello')
     })
 };
