fab99952077cd63e10418f9bca10a4d500ab8241
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-12 12:18:04 +0500

    consoling emailing and password

--- app/routes/registeration.js
+++ app/routes/registeration.js
@@ -2,7 +2,8 @@ module.exports = function(app, db) {
 
     app.post('/registration', (req, res) => {
         // You'll create 
-        console.log(req.body);
+        console.log(req.body.email);
+        console.log(req.body.password);
         console.log('sending data to App');
         res.send('Hello')
     })
