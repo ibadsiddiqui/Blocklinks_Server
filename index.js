2d7dee2d59e3a231be6666e83a3c56cb4875288d
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-12 12:17:38 +0500

    json parser and web3 imported

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// const Web3 = require('web3');
+// var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 
 const port = 8000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-require('./app/routes')(app,{});
 
-app.listen(port, ()=> {
-    console.log('We are live at ' + port);
+
+app.listen(port, async () => {
+    require('./app/routes')(app,{});
+    console.log('We are live on ' + port);
 })
