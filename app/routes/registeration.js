bfdff1e90edb9eca5eb35bf22eb43791d995faff
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-12 23:57:39 +0500

    registration for new account holders added and new address is available for creation

--- app/routes/registeration.js
+++ app/routes/registeration.js
@@ -1,10 +1,27 @@
-module.exports = function(app, db) {
 
-    app.post('/registration', (req, res) => {
-        // You'll create 
-        console.log(req.body.email);
-        console.log(req.body.password);
-        console.log('sending data to App');
-        res.send('Hello')
+module.exports = function(app, db) {
+    
+    const Web3 = require('web3');
+    var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+    var newAddress = "";
+
+    app.post('/registration', async (req, res) => {
+        // You'll create
+        
+        
+        console.log(req.body.UserID)
+        console.log(req.body.Email);
+        console.log(req.body.Password);
+        console.log(req.body.BusinessName);
+        console.log(req.body.FullName);
+        console.log(req.body.BusinessWeb);
+
+        var paraphrase = String(req.body.Email) + String(req.body.Password);        
+        console.log(paraphrase)
+
+        web3.eth.personal.newAccount(paraphrase)
+        .then(console.log);
+
+        res.send(newAddress)
     })
 };
