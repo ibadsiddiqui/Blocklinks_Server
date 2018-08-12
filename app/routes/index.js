b66b2e5e4bf98548743f3743b54220bbeb490969
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-12 12:36:58 +0500

    routes for login n email added to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,11 @@
 const noteRoutes = require('./routes_note');
 
-module.exports = function(app, db) {
-    noteRoutes(app, db);
+const loginRoutes = require('./login');
 
-  };
+const registrationRoutes = require('./registeration');
+
+module.exports = function(app, db) {
+  noteRoutes(app, db);
+  loginRoutes(app, db);
+  registrationRoutes(app, db);
+};
