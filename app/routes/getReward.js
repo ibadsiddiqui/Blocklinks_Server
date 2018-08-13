67e5299c0a453ece6585d560d5d25b976265422f
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-13 23:44:52 +0500

    unlocking account using the address

--- app/routes/getReward.js
+++ app/routes/getReward.js
@@ -3,19 +3,21 @@ module.exports = function(app, db) {
     const Web3 = require('web3');
     var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-    app.post('/login', async (req, res) => {
+    app.post('/getReward', async (req, res) => {
         
         
         // You'll create 
         console.log('Logging IN');
         console.log('Checking Email and Password');
-        console.log(req.body.email + " " + req.body.password);
-        var log = String(req.body.Email);
-        var password = String(req.body.Password);
-
-        web3.eth.personal.unlockAccount("0x634c9F966987b8e0dF385a6f94fB54FE1EdB4E94", log+password, 300, () => {
+        console.log(req.body.Email + " " + req.body.Password);
+        var address = String(req.body.Address);
+        var email = String(req.body.Email);
+        var password = String(req.body.Password)
+        web3.eth.personal.unlockAccount("0x634c9F966987b8e0dF385a6f94fB54FE1EdB4E94", email+password, 300, () => {
             console.log('Account unlocked!')
         });
+
+        // use the address to call the reward balance of the acccount
 
     })
 };
