26335e6329bf675a6d8e6aa018aca3caea5fcd29
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-20 11:18:00 +0500

    sending address of the DRW in response

--- app/routes/registeration.js
+++ app/routes/registeration.js
@@ -4,8 +4,9 @@ module.exports = function(app, db) {
     const Web3 = require('web3');
     var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
     var newAddress = "";
-
+    
     app.post('/registration', async (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
         // You'll create
         
         
@@ -19,9 +20,11 @@ module.exports = function(app, db) {
         var paraphrase = String(req.body.Email) + String(req.body.Password);        
         console.log(paraphrase)
 
-        web3.eth.personal.newAccount(paraphrase)
-        .then(console.log);
+        await web3.eth.personal.newAccount(paraphrase)
+        .then( result => res.send(JSON.stringify(result)));
 
-        res.send(newAddress)
+        
+
+        // res.send(newAddress) 
     })
 };
